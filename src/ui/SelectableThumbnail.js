49d1eac7d21ff121afdb63864d214433c933cac3
Mr Speaker <mrspeaker@gmail.com>
2017-04-21 12:38:16 -0400

    prettyfy

--- src/ui/SelectableThumbnail.js
+++ src/ui/SelectableThumbnail.js
@@ -2,17 +2,13 @@
 
 import React from "react";
 
-const {
-  Component,
-  PropTypes,
-} = React;
+const { Component, PropTypes } = React;
 
 class SelectableThumbnail extends Component {
-
   static propTypes = {
     src: PropTypes.string.isRequired,
     selected: PropTypes.bool.isRequired,
-    onToggle: PropTypes.func.isRequired,
+    onToggle: PropTypes.func.isRequired
   };
 
   state = {
@@ -20,26 +16,27 @@ class SelectableThumbnail extends Component {
     height: -1
   };
 
-  componentDidMount () {
+  componentDidMount() {
     this.refs.img.onload = () => {
-      this.refs.img && this.setState({
-        width: this.refs.img.naturalWidth,
-        height: this.refs.img.naturalHeight
-      });
+      this.refs.img &&
+        this.setState({
+          width: this.refs.img.naturalWidth,
+          height: this.refs.img.naturalHeight
+        });
     };
   }
 
-  render () {
+  render() {
     const { src, selected, onToggle } = this.props;
     const { width, height } = this.state;
 
-    return <div onClick={onToggle} className={`thumb ${selected ? "selected" :  ""}`}>
-      <img ref={"img"} src={src} title={ src.split("/").reverse()[0] } />
-      <div className="meta">{ width } x { height }</div>
-    </div>;
-
+    return (
+      <div onClick={onToggle} className={`thumb ${selected ? "selected" : ""}`}>
+        <img ref={"img"} src={src} title={src.split("/").reverse()[0]} />
+        <div className="meta">{width} x {height}</div>
+      </div>
+    );
   }
-
 }
 
 export default SelectableThumbnail;
